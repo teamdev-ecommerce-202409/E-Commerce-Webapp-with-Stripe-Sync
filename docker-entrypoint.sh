29172f53a511913a98accff548f5fa4d3d33c146
yadon <doinkya0@gmail.com>
2024-10-10 10:43:40 +0000

    Update workflow file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-/django/wait-for-it.sh db:3306 -t 0
+# 環境に応じて処理を分岐
+if [ "$DJANGO_ENV" = "production" ]; then
+    # RDSを使用している場合の処理 (本番環境)
+    until mysqladmin ping -h"${RDS_HOST}" -u"${RDS_USER}" -p"${RDS_PASSWORD}" --silent; do
+        echo 'Waiting for RDS MySQL to be ready...'
+        sleep 1
+    done
 
-if /django/venv/bin/python3 manage.py migrate; then
-    echo "Migration done successfully."
+    /django/venv/bin/python3 manage.py migrate
+    nginx -g 'daemon off;'
 else
-    echo "Failed to migrate." >&2
-    exit 1
+    # 開発環境の処理
+    /django/venv/bin/python3 manage.py runserver 0.0.0.0:8080
 fi
-
-if /django/venv/bin/python3 manage.py runserver 0.0.0.0:8080; then
-    echo "Server start on 0.0.0.0:8080"
-else
-    echo "Failed to start server." >&2
-    exit 1
-fi
-
-exec "$@"
